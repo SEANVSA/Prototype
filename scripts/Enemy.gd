04c9c05385564d64eda635a3e662f91e5531d40f
SEANVSA <seanvs06@gmail.com>
2025-06-27 19:27:01 +0700

    Update Enemy.gd
    ku tambahin untuk enemy

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,5 +1,23 @@
 class_name Enemy
 
-@export var name:String = "Titan"
-@export var max_hp:int = 1
-@export var hp:int = 1
+var name:String = "Titan"
+var max_hp:int = 1
+var hp:int = 1
+var boss:bool = false
+
+func _init(_name:String, _max_hp:int):
+	name = _name
+	max_hp = _max_hp
+	hp = max_hp
+	
+func takeDamage(damage:int) -> void:
+	hp -= damage
+	
+func isAlive() -> bool:
+	return hp>0
+	
+func isBoss() -> bool:
+	return boss
+	
+func setAsBoss():
+	boss = true
