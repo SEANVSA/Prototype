388fd1ba11e3b7659b51a1fd0d02cbc3714d357c
SEANVSA <seanvs06@gmail.com>
2025-06-28 22:17:02 +0700

    remake gamescene added controller

--- scripts/BaseGame/StartMenuController.gd
+++ scripts/BaseGame/StartMenuController.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		get_tree().change_scene_to_file("res://scenes/GameScene.tscn")
