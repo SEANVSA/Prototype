d50352a59e518549e77c3ddadc3565c91440e1dc
SEANVSA <seanvs06@gmail.com>
2025-06-28 23:20:58 +0700

    trying game manager

--- scripts/Gameplay/GameManager.gd
+++ scripts/Gameplay/GameManager.gd
@@ -1,26 +1,29 @@
 extends Node2D
 
-@onready var player = $Player
-@onready var monster = $Monster1
-@onready var camera = $Camera2D
-@onready var area = $Area2D
-@onready var monsterHpLabel = $Label
-
-var stage:int = 1
-
+var player: Player = null
+var current_enemy: Enemy = null
+var active_heroes: Array[Heroes] = []
+var stage:int
+var gold:Resources
+var enemies_per_stage: int
+var defeated_enemies_this_stage: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.set_player_data(Player.new("Player", 1, 0.5, 2))
-	monster.set_monster_data(Enemy.new("Slime", 100))
-	monsterHpLabel.text = "HP: " + str(monster.getMonster().getMaxHp()) +"/" + str(monster.getMonster().getHp())
-
+	current_enemy.set_monster_data(Enemy.new("Slime", 100))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		player.set_animation("attack")
-		monster.takeDamage(player.getPlayer().tap_damage())
-		monsterHpLabel.text = "HP: " + str(monster.getMonster().getMaxHp()) +"/" + str(monster.getMonster().getHp())
+		current_enemy.takeDamage(player.getPlayer().tap_damage())
 		
+func start_new_game():
+	gold = Resources.new(0)
+	stage = 1
+	enemies_per_stage = 10
+	defeated_enemies_this_stage = 0
+	
